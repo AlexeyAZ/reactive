f7a7ddfc4840a9bf775b5b6fba875b3663931296
Alexey <teos.nl@gmail.com>
2017-05-03 16:45:56 +0300

    90%

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -1,4 +1,5 @@
 $(function () {
+    var body = $("body");
 
     if ($('input[type="range"]').length) {
         $('input[type="range"]').rangeslider({
@@ -15,6 +16,78 @@ $(function () {
     $(".js-sec5-gallery").slick({
         adaptiveHeight: true
     });
+
+    body.on("click", ".header__nav-link", function (e) {
+        e.preventDefault();
+
+        var self = $(this);
+        var selfHref = self.attr("href");
+        var block = $(selfHref);
+
+        if ($(".thanks").length) {
+            window.location = "/index.html" + selfHref;
+        } else {
+            $("html, body").animate({
+                scrollTop: block.offset().top - $(".header").innerHeight()
+            }, 500, function () {
+                window.location.hash = selfHref;
+            });
+        }
+    });
+
+    function sec3ListAnimate() {
+        var decor = $(".js-sec3-decor");
+        var activeClass = "sec3__img-decor_show";
+        var moveClass = "sec3__img-decor_move";
+        var oldIndex = 0;
+        var animationStart = false;
+
+        decor.eq(0).addClass(activeClass);
+        $(".js-sec3-item").eq(0).addClass("sec3__list-item_active");
+
+        $(".js-sec3-item").hover(function () {
+
+            var self = $(this);
+            var selfIndex = self.index();
+            var hoverElem = decor.eq(selfIndex);
+
+            $(".sec3__list-item_active").removeClass("sec3__list-item_active");
+            self.addClass("sec3__list-item_active");
+            $("." + activeClass).removeClass(activeClass);
+            hoverElem.addClass(activeClass);
+
+            // if (selfIndex != oldIndex && animationStart == false) {
+            //     animationStart = true;
+            //     decor.eq(selfIndex).css({
+            //         transform: "translateX(0)",
+            //     });
+
+            //     decor.eq(oldIndex).addClass(moveClass);
+            //     decor.eq(oldIndex).on("transitionend", function() {
+
+            //         decor.eq(oldIndex).removeClass(activeClass);
+            //         decor.eq(oldIndex).removeClass(moveClass);
+
+            //         $(this).off("transitionend");
+            //         animationStart = false;
+            //         oldIndex = selfIndex;
+            //     })
+
+            //     decor.eq(selfIndex).addClass(activeClass);
+            //     decor.eq(selfIndex).attr("style", "");
+
+            //     console.log(oldIndex)
+            // }
+        }, function () {
+            var self = $(this);
+            var selfIndex = self.index();
+            var hoverElem = decor.eq(selfIndex);
+        });
+    }
+
+    if (window.matchMedia("(max-width: 1024px)").matches) {} else {
+        sec3ListAnimate();
+    }
 
     function phoneLink() {
         var md = new MobileDetect(window.navigator.userAgent);
@@ -33,12 +106,20 @@ $(function () {
     phoneLink();
 
     // form handler
-    var body = $("body");
     var name;
 
     $("input[name=phone]").inputmask({
         "mask": "+9(999)999-9999",
         greedy: false
+    });
+
+    body.on("click", ".js-modal", function (e) {
+        e.preventDefault();
+
+        var self = $(this);
+        var modal = self.data("modal");
+        $(".modal-" + modal).fadeIn();
+        $("html").addClass("form-open");
     });
 
     body.on("click", ".js-small-btn", function (e) {
@@ -53,7 +134,9 @@ $(function () {
     body.on("click", function (e) {
         var self = $(e.target);
 
-        if (self.hasClass("form-wrap_small") || self.hasClass("form__close")) {
+        if (self.hasClass("form-wrap_small") || self.hasClass("form__close") || self.hasClass("modal")) {
+            $("#videoFrame")[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+            $(".modal").fadeOut();
             $("html").removeClass("form-open");
             $(".form-wrap").removeClass("form-wrap_open");
         } else if (self.hasClass("form-wrap_big")) {
@@ -96,7 +179,7 @@ $(function () {
             }
         };
     } else {
-        $("#smallForm, #bottomForm").submit(function (e) {
+        $("#smallForm, #bottomForm, #openForm").submit(function (e) {
             e.preventDefault();
             $(".form-wrap_open").removeClass("form-wrap_open");
 
@@ -155,5 +238,31 @@ $(function () {
     if ($("#thanksName").length) {
         $("#thanksName").text(localStorage.getItem("landclientname"));
     };
+
+    function fixHeader() {
+        var topScroll = $(document).scrollTop();
+        var header = $(".header");
+        var headerHeight = header.innerHeight();
+
+        if (!window.matchMedia("(max-width:1024px)").matches) {
+            header.removeClass("header_mobile");
+            header.css("top", headerHeight);
+
+            if (topScroll > headerHeight * 8) {
+                header.addClass("header_fixed").css("top", 0);
+            } else {
+                header.removeClass("header_fixed").css("top", headerHeight);
+            }
+        } else {
+            header.removeClass("header_fixed");
+            header.addClass("header_mobile");
+            header.attr("style", "");
+        }
+    }
+    fixHeader();
+
+    $(window).on("scroll resize", function () {
+        fixHeader();
+    });
 });
 //# sourceMappingURL=app.js.map
